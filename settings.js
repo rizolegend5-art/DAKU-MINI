8ccf3b9952fe05b8806342a39553e4ced709a31c
rizolegend5-art <rizolegend5@gmail.com>
2026-08-06 06:05:49 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/446b30.png',
+    ownerNumber: process.env.OWNER_NUMBER || '923713809660',
+    botName: 'DAKU MINI BOT',
+    ownerName: 'RIZO X SHAKOR',
+    whatsappChannel: 'https://whatsapp.com/channel/0029VbDg0PKISTkFbLhqfl0C',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8265653257',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
